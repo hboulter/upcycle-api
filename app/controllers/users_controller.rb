6b8c00c3397db8bbceeede4b1c71253a4200e833
Heidi Boulter <boulter.heidi@gmail.com>
2020-02-04 16:39:46 -0600

    active storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,11 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
     def create
         user = User.create!(
             username: params['user']['username'],
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :show, :index] # registration
-  resources :posts, only: [:create, :show, :index]
+  resources :posts
   resources :sessions, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
+  post "/rails/active_storage/direct_uploads", to: "direct_uploads#create"
 end
